1e66837a83f388278f79336b9b5ed79d10d6abed
Osmar Bruno <100640137+BRun0442@users.noreply.github.com>
2022-06-12 13:13:33 -0300

    Arquivos do site

---
+++ Doguitos/js/index.js
@@ -0,0 +1,8 @@
+function scrollUp()
+{
+  // document.body.scrollTop = 0;
+  // document.documentElement.scrollTop = 0;
+
+  //Subir a página quando clicar no botão         
+  window.scrollTo({top: 0, behavior: 'smooth'});
+}
